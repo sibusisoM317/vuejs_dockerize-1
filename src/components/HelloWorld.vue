533cc5985aa2d2c016a72c1afa765c5f2450f9c7
Igor Cruz <igor_cruz_avelino@hotmail.com>
2020-05-05 16:40:57 -0300

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,9 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+    <br>
+    <h1>I am the master o fu universe hsduahuhaushahuaha</h1>
+    <br>
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
